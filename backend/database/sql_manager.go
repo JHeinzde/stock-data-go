53f2b5132d25d511f954fd9a1516d0e702a6bd57
jheinz <heinzjonathan95@gmail.com>
2019-12-25 00:15:15 +0100

    First implementation of crud for database

--- backend/database/sql_manager.go
+++ backend/database/sql_manager.go
@@ -1,1 +1,44 @@
 package database
+
+import "github.com/gin-gonic/gin"
+import "github.com/jinzhu/gorm"
+
+func InsertUser(user User, c *gin.Context) {
+	tx := getTransactionFromContext(c)
+
+	tx.Create(&user)
+}
+
+func InsertStock(symbol string, c *gin.Context) {
+	stock := Stock{Symbol: symbol}
+	tx := getTransactionFromContext(c)
+
+	tx.Create(&stock)
+}
+
+func RemoveUser(user User, c *gin.Context) {
+	tx := getTransactionFromContext(c)
+
+	tx.Delete(&user)
+}
+
+func RemoveStock(symbol string, c *gin.Context) {
+	stock := Stock{Symbol: symbol}
+	tx := getTransactionFromContext(c)
+
+	tx.Delete(&stock)
+}
+
+func GetAllStocks(c *gin.Context) *[]Stock {
+	tx := getTransactionFromContext(c)
+	stocks := make([]Stock, 0)
+
+	tx.Find(&stocks)
+
+	return &stocks
+}
+
+func getTransactionFromContext(c *gin.Context) *gorm.DB {
+	tx := c.MustGet("transaction")
+	return tx.(*gorm.DB)
+}
--- backend/database/sql_structs.go
+++ backend/database/sql_structs.go
@@ -1,4 +1,14 @@
 package database
 
+import "github.com/jinzhu/gorm"
+
 type User struct {
+	gorm.Model
+	Username string `gorm:"unique;not null"`
+	Password string `gorm:"not null"`
+}
+
+type Stock struct {
+	gorm.Model
+	Symbol string `gorm:"unique;not null"`
 }
